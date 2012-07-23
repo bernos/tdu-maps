fd6ab78da8690c49775e214b723338bc10dabed1
unknown <mcmahonb@AU-0600679.ap.corp.yr.com>
2012-07-23 17:37:10 +1000

    added updated grunt config

--- grunt.js
+++ grunt.js
@@ -34,6 +34,86 @@ module.exports = function(grunt) {
         }
       }
     },
+
+    fetchFeeds : {
+      all: {
+        dest : 'src/api',
+        host : 'tourdownunder.com.au',
+        feeds : {
+          '/tdu-media/helpers/results.ashx?stage=O&jersey=SPR' : 'results/0/SPR.json',
+          '/tdu-media/helpers/results.ashx?stage=O&jersey=YNG' : 'results/0/YNG.json',
+          '/tdu-media/helpers/results.ashx?stage=O&jersey=LDT' : 'results/0/LDT.json',
+          '/tdu-media/helpers/results.ashx?stage=O&jersey=SAN' : 'results/0/SAN.json',
+          '/tdu-media/helpers/results.ashx?stage=O&jersey=KOM' : 'results/0/KOM.json',
+          '/tdu-media/helpers/results.ashx?stage=O&jersey=MAR' : 'results/0/MAR.json',
+
+          '/tdu-media/helpers/results.ashx?stage=C&jersey=CLA' : 'results/C/CLA.json',
+          '/tdu-media/helpers/results.ashx?stage=C&jersey=PR1' : 'results/C/PR1.json',
+          '/tdu-media/helpers/results.ashx?stage=C&jersey=PR2' : 'results/C/PR2.json',
+          '/tdu-media/helpers/results.ashx?stage=C&jersey=PR3' : 'results/C/PR3.json',
+          '/tdu-media/helpers/results.ashx?stage=C&jersey=PR4' : 'results/C/PR4.json',
+
+          '/tdu-media/helpers/results.ashx?stage=1&jersey=SAN' : 'results/1/SAN.json',
+          '/tdu-media/helpers/results.ashx?stage=1&jersey=KOM' : 'results/1/KOM.json',
+          '/tdu-media/helpers/results.ashx?stage=1&jersey=SPR' : 'results/1/SPR.json',
+          '/tdu-media/helpers/results.ashx?stage=1&jersey=YNG' : 'results/1/YNG.json',
+          '/tdu-media/helpers/results.ashx?stage=1&jersey=MAR' : 'results/1/MAR.json',
+          '/tdu-media/helpers/results.ashx?stage=1&jersey=LDT' : 'results/1/LDT.json',
+          '/tdu-media/helpers/results.ashx?stage=1&jersey=STG' : 'results/1/STG.json',
+
+          '/tdu-media/helpers/results.ashx?stage=2&jersey=SAN' : 'results/2/SAN.json',
+          '/tdu-media/helpers/results.ashx?stage=2&jersey=KOM' : 'results/2/KOM.json',
+          '/tdu-media/helpers/results.ashx?stage=2&jersey=SPR' : 'results/2/SPR.json',
+          '/tdu-media/helpers/results.ashx?stage=2&jersey=YNG' : 'results/2/YNG.json',
+          '/tdu-media/helpers/results.ashx?stage=2&jersey=MAR' : 'results/2/MAR.json',
+          '/tdu-media/helpers/results.ashx?stage=2&jersey=LDT' : 'results/2/LDT.json',
+          '/tdu-media/helpers/results.ashx?stage=2&jersey=STG' : 'results/2/STG.json',
+
+          '/tdu-media/helpers/results.ashx?stage=3&jersey=SAN' : 'results/3/SAN.json',
+          '/tdu-media/helpers/results.ashx?stage=3&jersey=KOM' : 'results/3/KOM.json',
+          '/tdu-media/helpers/results.ashx?stage=3&jersey=SPR' : 'results/3/SPR.json',
+          '/tdu-media/helpers/results.ashx?stage=3&jersey=YNG' : 'results/3/YNG.json',
+          '/tdu-media/helpers/results.ashx?stage=3&jersey=MAR' : 'results/3/MAR.json',
+          '/tdu-media/helpers/results.ashx?stage=3&jersey=LDT' : 'results/3/LDT.json',
+          '/tdu-media/helpers/results.ashx?stage=3&jersey=STG' : 'results/3/STG.json',
+
+          '/tdu-media/helpers/results.ashx?stage=4&jersey=SAN' : 'results/4/SAN.json',
+          '/tdu-media/helpers/results.ashx?stage=4&jersey=KOM' : 'results/4/KOM.json',
+          '/tdu-media/helpers/results.ashx?stage=4&jersey=SPR' : 'results/4/SPR.json',
+          '/tdu-media/helpers/results.ashx?stage=4&jersey=YNG' : 'results/4/YNG.json',
+          '/tdu-media/helpers/results.ashx?stage=4&jersey=MAR' : 'results/4/MAR.json',
+          '/tdu-media/helpers/results.ashx?stage=4&jersey=LDT' : 'results/4/LDT.json',
+          '/tdu-media/helpers/results.ashx?stage=4&jersey=STG' : 'results/4/STG.json',
+
+          '/tdu-media/helpers/results.ashx?stage=5&jersey=SAN' : 'results/5/SAN.json',
+          '/tdu-media/helpers/results.ashx?stage=5&jersey=KOM' : 'results/5/KOM.json',
+          '/tdu-media/helpers/results.ashx?stage=5&jersey=SPR' : 'results/5/SPR.json',
+          '/tdu-media/helpers/results.ashx?stage=5&jersey=YNG' : 'results/5/YNG.json',
+          '/tdu-media/helpers/results.ashx?stage=5&jersey=MAR' : 'results/5/MAR.json',
+          '/tdu-media/helpers/results.ashx?stage=5&jersey=LDT' : 'results/5/LDT.json',
+          '/tdu-media/helpers/results.ashx?stage=5&jersey=STG' : 'results/5/STG.json',
+
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=SAN' : 'results/6/SAN.json',
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=KOM' : 'results/6/KOM.json',
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=SPR' : 'results/6/SPR.json',
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=YNG' : 'results/6/YNG.json',
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=MAR' : 'results/6/MAR.json',
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=LDT' : 'results/6/LDT.json',
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=STG' : 'results/6/STG.json',
+          '/tdu-media/helpers/results.ashx?stage=6&jersey=TDA' : 'results/6/TDA.json',
+
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=1' : 'livefeeds/1.json', 
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=3' : 'livefeeds/3.json',
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=4' : 'livefeeds/4.json',
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=5' : 'livefeeds/5.json',
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=6' : 'livefeeds/6.json',
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=7' : 'livefeeds/7.json',
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=8' : 'livefeeds/8.json',
+          '/tdu-media/helpers/racefeed.ashx?maxItems=30&feedID=9' : 'livefeeds/9.json'
+
+        }
+      }
+    },
     
     requirejs: {
       baseUrl: "src/js",
